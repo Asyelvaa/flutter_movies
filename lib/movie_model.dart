a8140969587d225a912fb49bb50dd0613ddad361
Asyelvaa <asyelvaa@gmail.com>
2023-08-13 01:02:03 +0700

    first commit

---
+++ lib/movie_model.dart
@@ -0,0 +1,12 @@
+class Movie {
+  final String name;
+  final int releaseYear;
+  final double rating;
+  final String posterUrl;
+
+  Movie({
+    required this.name, 
+    required this.releaseYear, 
+    required this.rating, 
+    required this.posterUrl});
+}
